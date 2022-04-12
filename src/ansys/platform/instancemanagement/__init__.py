b59049ad445acd4bcf0fa9aa5d93a21c018e6c60
Pierre Lulé <pierre.lule@ansys.com>
2022-04-12 16:02:27 +0200

    Unattended connection (#28)
    This PR introduces a very basic configuration format where, by default, a connection made to the PIM would be configured not through code but through a configuration file injected into the environment.
    
    The client code is then as simple as:
    
    ```py
    client = pypim.connect()
    client.definitions(product_name="hello-world", product_version="231")
    ```
    
    This is intended to support cloud scenarios, where containers would be created with configuration files injected into them, without modifying the client code. This does not close the door to application managing themselves the gRPC channel though.
    
    The configuration file is a json file, in order to keep parsing it as simple as possible, even in other languages. The "switch" to enable the interaction with PIM is by setting the environment variable `ANSYS_PLATFORM_INSTANCEMANAGEMENT_CONFIG`. 
    
    The configuration file format is:
    
    ```json
    {
        "version": 1,
        "pim": {
            "uri": "dns:127.0.0.1:5000",
            "headers": {
                "some": "header"
            },
            "tls": false
        }
    }
    ```

--- src/ansys/platform/instancemanagement/__init__.py
+++ src/ansys/platform/instancemanagement/__init__.py
@@ -5,9 +5,36 @@ try:
 except ModuleNotFoundError:
     import importlib_metadata
 
-__version__ = importlib_metadata.version(__name__.replace(".", "-"))
+import os
 
 from ansys.platform.instancemanagement.client import Client
 from ansys.platform.instancemanagement.definition import Definition
 from ansys.platform.instancemanagement.instance import Instance
 from ansys.platform.instancemanagement.service import Service
+
+__version__ = importlib_metadata.version(__name__.replace(".", "-"))
+
+CONFIGURATION_PATH_ENVIRONMENT_VARIABLE = "ANSYS_PLATFORM_INSTANCEMANAGEMENT_CONFIG"
+
+
+def is_configured() -> bool:
+    """Check if the environment is configured to use PyPIM.
+
+    Returns:
+        bool: True if the environment is configured to use PyPIM.
+    """
+    return CONFIGURATION_PATH_ENVIRONMENT_VARIABLE in os.environ
+
+
+def connect() -> Client:
+    """Create a PyPIM client based on the environment configuration.
+
+    Raises:
+        RuntimeError: The environment is not configured to use PyPIM
+
+    Returns:
+        Client: A PyPIM client, the main entrypoint to use this library.
+    """
+    if not is_configured():
+        raise RuntimeError("The environment is not configured to use PyPIM.")
+    return Client._from_configuration(os.environ[CONFIGURATION_PATH_ENVIRONMENT_VARIABLE])
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,6 @@
 from concurrent.futures import ThreadPoolExecutor
-from unittest.mock import MagicMock
+import os
+from unittest.mock import MagicMock, patch
 
 from ansys.api.platform.instancemanagement.v1.product_instance_manager_pb2 import (
     Definition as DefinitionV1,
@@ -8,10 +9,13 @@ from ansys.api.platform.instancemanagement.v1.product_instance_manager_pb2 impor
     ListDefinitionsRequest,
     ListDefinitionsResponse,
 )
+import ansys.api.platform.instancemanagement.v1.product_instance_manager_pb2_grpc as product_instance_manager_pb2_grpc  # noqa: E501
+import grpc
 from grpc import StatusCode
 import grpc_testing
 import pytest
 
+import ansys.platform.instancemanagement as pypim
 from ansys.platform.instancemanagement import Client, Definition, Instance
 from conftest import LIST_DEFINITIONS_METHOD
 
@@ -163,3 +167,55 @@ def test_create_instance(testing_channel):
     definitions[0].create_instance.assert_called_once_with(timeout=0.32)
     definitions[1].create_instance.assert_not_called()
     assert created_instance == instance
+
+
+def test_initialize_from_configuration(testing_pool, tmp_path):
+    # Arrange
+    # A basic implementation of PIM
+    received_metadata = []
+
+    class PIMServicer(product_instance_manager_pb2_grpc.ProductInstanceManagerServicer):
+        def ListDefinitions(self, _, context):
+            received_metadata.append(context.invocation_metadata())
+            return ListDefinitionsResponse()
+
+    server = grpc.server(testing_pool)
+    product_instance_manager_pb2_grpc.add_ProductInstanceManagerServicer_to_server(
+        PIMServicer(), server
+    )
+
+    port = server.add_insecure_port("127.0.0.1:0")
+    server.start()
+    # A valid configuration file setting up the uri and metadata
+    config_path = str(tmp_path / "config.json")
+    config = (
+        r"""{
+    "version": 1,
+    "pim": {
+        "uri": "dns:127.0.0.1:%s",
+        "headers": {
+            "token": "007",
+            "identity": "james bond"
+        },
+        "tls": false
+    }
+}"""
+        % port
+    )
+
+    with open(config_path, "w") as f:
+        f.write(config)
+
+    # Act
+    # Connect the client based on this configuration
+    # and run a request
+    with patch.dict(os.environ, {"ANSYS_PLATFORM_INSTANCEMANAGEMENT_CONFIG": config_path}):
+        client = pypim.connect()
+        client.definitions(product_name="hello-world", product_version="231")
+
+    # Assert
+    # The server got the request with the intended headers
+    assert len(received_metadata) == 1
+    received_metadata_dict = dict(received_metadata[0])
+    assert received_metadata_dict["token"] == "007"
+    assert received_metadata_dict["identity"] == "james bond"
