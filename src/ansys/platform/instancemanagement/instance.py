ddaa87787d4558b4488a9a5dd62ad1021c1d1b6b
Mathilde <mathilde.gallouet@ansys.com>
2023-03-13 09:03:53 +0100

    Update src/ansys/platform/instancemanagement/instance.py
    Co-authored-by: Pierre Lulé <pierre.lule@ansys.com>

--- src/ansys/platform/instancemanagement/instance.py
+++ src/ansys/platform/instancemanagement/instance.py
@@ -287,7 +287,7 @@ class Instance(contextlib.AbstractContextManager):
 
         configuration = Configuration.from_environment()
         if configuration.tls:
-            return service._build_grpcs_channel(configuration, **kwargs)
+            return service._build_grpcs_channel(configuration=configuration, **kwargs)
         else:
             return service._build_grpc_channel(**kwargs)
 
