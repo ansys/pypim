b29482d22f5a8c2745040e2083b1480932e256e1
Revathy Venugopal <104772255+Revathyvenugopal162@users.noreply.github.com>
2023-04-27 16:31:22 +0200

    Doc: remove the templates for fixing the broken docs. (#80)
    * Doc: remove the templates
    
    * Doc: add the python version
    
    * Doc: add the python version
    
    * Doc: update the pre-commit
    
    * Doc: update the tox tests
    
    * Doc: ufix typo
    
    * Doc: fix pre-commit
    
    * Update .pre-commit-config.yaml

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -80,9 +80,6 @@ html_static_path = ["_static"]
 # Include the cname in the generated documentation.
 html_extra_path = ["CNAME"]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
 # The suffix(es) of source filenames.
 source_suffix = ".rst"
 
